e2123cedbe7e878087322761620a485ef7dab02b
taegeon <g7193g@naver.com>
2025-01-26 21:35:14 +0900

    [create] create project

---
+++ src/test/java/com/example/live_tino/LiveTinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.live_tino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveTinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
